0657bdfc97a97df7a44468928b2a2c0676168e51
Bryan Ayala <blayala@cpp.edu>
2018-10-08 10:33:13 -0700

    added loading page

--- src/client/components/HomePage.js
+++ src/client/components/HomePage.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './componentStyles/HomePage.css';
+import LoadingPage from './LoadingPage';
 
 export default class HomePage extends Component {
   render() {
     return (
-        <div className="mainBody">
-          <h1 className="pageHeader">Home Page</h1>
-        </div>
+      <div className="mainBody">
+        <h1 className="pageHeader">Home Page</h1>
+      </div>
     );
   }
 }
---
+++ src/client/components/LoadingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './componentStyles/loadingPage.css';
+
+const loadingPage = () => (
+  <div className="container">
+    <div className="dash uno" />
+    <div className="dash dos" />
+    <div className="dash tres" />
+    <div className="dash cuatro" />
+  </div>
+);
+
+export default loadingPage;
