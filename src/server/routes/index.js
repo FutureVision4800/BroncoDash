c79cd6631107a2807727abd96ee741ad5379caa8
C.R.A.S.H <mardirossian.sevan@gmail.com>
2018-10-28 00:03:54 -0700

    backend- progress
    OK so I made a mysql databases the conntection for backend has some bugs that needs to be fixed but it doesn't effect the forntend the ports for are
    localhost:3000 frontend
    localhost:3001 backend
    for running mysql use XAMPP for it this provide apache and mysql for it to run PHPmyadmin

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+    connection.query('SELECT * FROM employees', (err,rows) => {
+        if(!err) throw err;
+
+        console.log('Data received from Db:\n');
+        console.log(rows);
+    });
+});
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'mydb'
+});
+connection.connect((err) => {
+    if (!err) throw err;
+    console.log('Connected!');
+});
+
+//how to create data
+const employee = { name: 'Winnie', location: 'Australia' };
+connection.query('INSERT INTO employees SET ?', employee, (err, res) => {
+    if(!err) throw err;
+
+    console.log('Last insert ID:', res.insertId);
+});
+
+//how to update location
+connection.query(
+    'UPDATE employees SET location = ? Where ID = ?',
+    ['South Africa', 5],
+    (err, result) => {
+        if (!err) throw err;
+
+        console.log(`Changed ${result.changedRows} row(s)`);
+    }
+);
+//how to delete
+connection.query(
+    'DELETE FROM employees WHERE id = ?', [5], (err, result) => {
+        if (!err) throw err;
+
+        console.log(`Deleted ${result.affectedRows} row(s)`);
+    }
+);
+
 
 module.exports = router;
